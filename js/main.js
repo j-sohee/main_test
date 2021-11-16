fffd90844caad288fedc70e4a5612e1f5e669239
j-sohee <sohee.jin@gmail.com>
2021-11-16 23:27:50 +0900

    main 구조변경 및 모션 진행

--- js/main.js
+++ js/main.js
@@ -1,84 +1,58 @@
-//header 2depth menu
-$("#gnb>li").on("mouseenter", function(){
-    $(this).find(".gnb_2depth").show();
-})
-$("#gnb>li").on("mouseleave", function(){
-    $(this).find(".gnb_2depth").hide();
-})
+const $visual = $(".visual");
+const $mainTit = $visual.find("#mainTit")
+const $mainTit_len = $mainTit.length;
+const $currentIndex = 0;
+const $mainVisual = $(".visual2 .mainVisual")
+const $next = $(".next");
+const $prev = $(".prev");
+let tit = 0;
 
-$("#gnb>li").each(function(index){
-    $("#gnb>li").eq(index).find("a").on("focusin", function(){
-        $("#gnb>li").eq(index).find(".gnb_2depth").show();     
-    });
-
-    $("#gnb>li").eq(index).find("a").last().on("focusout", function(){
-        $("#gnb>li").eq(index).find(".gnb_2depth").hide();
-    });
-});
-
-//gnbMo
-var btnCall = document.querySelector(".btnCall");
-var menuMo = document.querySelector(".menuMo");
-
-btnCall.onclick = function(e){
-    e.preventDefault();
-    btnCall.classList.toggle("on");
-    menuMo.classList.toggle("on");
-}
-
-//main slider
-const $slider = $("#visual").find("#slider");
-const $next = $("#visual").find(".next");
-const $prev = $("#visual").find(".prev");
-const $pic1 = $("#visual").find(".right>.pic")
-const $pic2 = $("#visual").find(".mainVisual>.pic");
-const $miniTxt = $slider.find(".wrap");
-const $mainTit = $slider.find(".mainTit");
-const $line = $slider.find(".right>.line");
-let speed = 500;
-
-
-$slider.children("ul").find("li").last().prependTo($slider.children("ul"));
-
-//main motion
-motion();
-
+$mainVisual.find("li").last().prependTo($mainVisual);
 
 $next.on("click", function(e){
     e.preventDefault();
-    next($slider);
-    motion();
+
+    $mainVisual.animate({marginLeft : "-200%"}, 500,function(){
+        $mainVisual.css({marginLeft:"-100%"});
+        $mainVisual.find("li").first().appendTo(this);
+    });
+
+    $mainTit.find("li.on").addClass("upper");
+    
+    setTimeout(function(){
+        $mainTit.find("li").removeClass("on");
+        $mainTit.find("li").removeClass("upper");
+        if(tit<2){
+            $mainTit.find("li").eq(tit + 1).addClass("on");
+            tit++;
+        }else{
+            tit = 0;
+            $mainTit.find("li").eq(tit).addClass("on");
+        }
+    },500)
 });
 
 $prev.on("click", function(e){
     e.preventDefault();
-    prev($slider);
+
+    $mainVisual.animate({marginLeft : "0%"}, 500,function(){
+        $mainVisual.css({marginLeft:"-100%"});
+        $mainVisual.find("li").last().prependTo(this);
+    });
+
+    $mainTit.find("li.on").addClass("upper");
+    
+    setTimeout(function(){
+        $mainTit.find("li").removeClass("on");
+        $mainTit.find("li").removeClass("upper");
+        if(tit>0){
+            $mainTit.find("li").eq(tit-1).addClass("on");
+            tit--;
+            
+        }else{
+            tit = 2;
+            $mainTit.find("li").eq(tit).addClass("on");
+        }
+        console.log(tit);
+    },500)
 });
-
-function motion(){
-    $line.animate({ marginTop:0, height:"378px" }, speed, function(){
-        $pic1.animate({ height:"160px", marginTop:0},speed, function(){
-            $pic2.animate({ height:"100%", marginTop:0},speed, function(){
-                $miniTxt.addClass("on");
-                $mainTit.addClass("on");
-                $prev.addClass("on");
-                $next.addClass("on");
-            });
-        });
-    });
-}
-
-function next(el){
-    el.children("ul").animate({marginLeft : "-200%"},speed, function(){
-        $(this).css({marginLeft : "-100%"});
-        $(this).children("li").first().appendTo(this);
-    });
-}
-
-function prev(el){
-    el.children("ul").animate({marginLeft:0}, speed, function(){
-        $(this).css({marginLeft:"-100%"});
-        $(this).children("li").last().prependTo(this);
-    });
-}
- 
